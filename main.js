8abcddffdf441d4084d7429531ddcb89fc68788d
anxo-cloud <anxobreoganFuentes@gmail.com>
2019-11-17 23:24:09 +0100

    function sayMe ok!!

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert ('hola');
+const inputData=document.getElementById('entrada');
+const boton=document.getElementById('boton');
+
+function sayMe (){
+    console.log(inputData.value);
+}
+
+boton.addEventListener('click',sayMe);
